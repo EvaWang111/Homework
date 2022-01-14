5334a95c911f41f562929b1b027d3eb35ae19098
wangyiyun <eva.wangyy@qq.com>
2022-01-15 00:39:34 +0800

    提交第二周作业

---
+++ src/main/java/com/example/Homework/week02/HttpServer.java
@@ -0,0 +1,46 @@
+package com.example.Homework.week02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.rmi.server.ExportException;
+
+@SpringBootApplication
+public class HttpServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8801);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                service(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void service(Socket socket) {
+        try {
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            String body = "hello!!!!";
+            printWriter.println("Content-Length:" + body.getBytes().length);
+
+            printWriter.println();
+            printWriter.write(body);
+            printWriter.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
